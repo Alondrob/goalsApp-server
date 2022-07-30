8730d79ffdd91f99f361dbd6622c1b229dccb3f1
Alondrob <alondrob@gmail.com>
2022-07-30 12:02:00 -0400

    last working version

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log('hello')
+const express = require('express');
+const dotnev = require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));
