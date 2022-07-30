e5cd5f62f073d71df51a50f1ec75305375acbba4
Alondrob <alondrob@gmail.com>
2022-07-30 15:58:41 -0400

    thirdOne

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,24 @@
+const getGoals = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+const setGoal = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+const updateGoal = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+
+
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+// console.log(app)
 app.use('/api/goals', require('./routes/goalRoutes'))
 
 
